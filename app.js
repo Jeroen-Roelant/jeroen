a516b2b96ee99da499ca7bd0adb8ea358fd59f07
Jeroen Roelant <jeroenroelant@hotmail.com>
2024-05-25 22:09:35 +0200

    server

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var serveStatic = require('serve-static');
 
 connect()
     .use(serveStatic(__dirname))
-    .listen(8080, () => console.log('Server running'));
+    .listen(3000, () => console.log('Server running'));
