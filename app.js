abaab00480ec0784fd67bdade53a07e753a19814
Jeroen Roelant <jeroenroelant@hotmail.com>
2024-05-28 16:48:51 +0200

    portfolio

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect()
-    .use(serveStatic(__dirname))
+    .use(serveStatic(__dirname + '/portfolio'))
     .listen(process.env.PORT || 3000, () => console.log('Server running'));
