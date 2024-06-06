06f745da435a7cc6ce2888c7339aa309b42dfbcd
Jeroen Roelant <jeroenroelant@hotmail.com>
2024-06-06 17:53:47 +0200

    redirect

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var serveStatic = require('serve-static');
 
 connect()
     .use(serveStatic(__dirname + '/portfolio'))
+    .use('/cv', function(req, res, next) {
+        res.writeHead(301, { 'Location': '/cv_JeroenRoelant.pdf' });
+        res.end();
+    })
     .listen(process.env.PORT || 3000, () => console.log('Server running on ' + (process.env.PORT || 3000)));
